b20eaf95c3b5dbdc9d7ec4a6d103a27df2b1c45a
John Kim <kimjohndev@gmail.com>
2025-03-30 17:54:18 -0400

    Time: 7 ms (44.85%), Space: 19.2 MB (57.62%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,20 +1,14 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        ans, currPath = [], [0]
-        # iterate through every node
-    
-        # we can read the last variable and return that path
-        # i represents the index we're going to on the graph
-        def backtrack(currPath, i):
-            if len(currPath) >= 1 and currPath[-1] == len(graph) - 1:
-                ans.append(currPath.copy())
+        ans, currList = [], [0]
+        def backtrack(currList, i):
+            if i == len(graph) - 1:
+                ans.append(currList.copy())
                 return
-            # have a for loop for the first index (which represents what 0 can go to )
-            # this double adds
-            for num in graph[i]:
-                 # we should first add it
-                currPath.append(num)
-                backtrack(currPath, num)
-                currPath.pop()
-        backtrack(currPath, 0)
+            nums = graph[i]
+            for num in nums:
+                currList.append(num)
+                backtrack(currList, num)
+                currList.pop()
+        backtrack(currList, 0)
         return ans
