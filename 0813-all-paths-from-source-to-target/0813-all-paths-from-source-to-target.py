de0cdb00828365df6d7b5c86f33427416e841e71
John Kim <kimjohndev@gmail.com>
2025-04-03 11:13:21 -0400

    Time: 4 ms (69.62%), Space: 19.4 MB (30.34%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,14 +1,20 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        ans, currList = [], [0]
-        def backtrack(currList, i):
-            if i == len(graph) - 1:
-                ans.append(currList.copy())
+        # graph[i] is a list of all nodes you can visit from node i
+        n = len(graph)
+        # i represents the index that we are looking at 
+        def backtrack(i, currPath):
+            # what's the constraint
+            if currPath[-1] == n - 1:
+                ans.append(currPath.copy())
                 return
             nums = graph[i]
             for num in nums:
-                currList.append(num)
-                backtrack(currList, num)
-                currList.pop()
-        backtrack(currList, 0)
+                currPath.append(num)
+                backtrack(num, currPath)
+                currPath.pop()
+            
+        ans = []
+        backtrack(0, [0])
         return ans
+
