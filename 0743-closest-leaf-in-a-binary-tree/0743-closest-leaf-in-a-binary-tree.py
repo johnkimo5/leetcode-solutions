3959c4290b09c2bebf95565bb44fe718395e9034
John Kim <kimjohndev@gmail.com>
2025-09-05 22:14:24 -0400

    Time: 3 ms (82.12%), Space: 18.3 MB (74.37%) - LeetHub

--- 0743-closest-leaf-in-a-binary-tree/0743-closest-leaf-in-a-binary-tree.py
+++ 0743-closest-leaf-in-a-binary-tree/0743-closest-leaf-in-a-binary-tree.py
@@ -14,31 +14,30 @@ this requires us to create an undirected graph from the tree with edges going to
 from collections import deque, defaultdict
 class Solution:
     def findClosestLeaf(self, root: Optional[TreeNode], k: int) -> int:
-        adjList = defaultdict(list)
-        q = deque([root])
-        leaves = set()
+        parent = {}
+        start = None
+        def dfs(curr):
+            nonlocal start
+            if not curr:
+                return
+            if curr.val == k:
+                start = curr
+            if curr.left:
+                parent[curr.left] = curr
+            if curr.right:
+                parent[curr.right] = curr
+            dfs(curr.left)
+            dfs(curr.right)
+
+        dfs(root)
+
+        q = deque([start])
+        visited = set([start])
         while q:
             node = q.popleft()
             if not node.left and not node.right:
-                leaves.add(node.val)
-            if node.left:
-                adjList[node.val].append(node.left.val)
-                adjList[node.left.val].append(node.val)
-                q.append(node.left)
-            if node.right:
-                adjList[node.val].append(node.right.val)
-                adjList[node.right.val].append(node.val)
-                q.append(node.right)
-        print(adjList)
-        q = deque([k])
-        visited = set([k])
-        print(q)
-        print(visited)
-        while q:
-            node = q.popleft()
-            if node in leaves:
-                return node
-            for neighbor in adjList[node]:
-                if neighbor not in visited:
+                return node.val
+            for neighbor in (node.left, node.right, parent.get(node)):
+                if neighbor and neighbor not in visited:
                     visited.add(neighbor)
                     q.append(neighbor)
